a3a93e8bbe862b4437d5e3a97ae50a92acde0ee5
Rajaguru_241 <86597128+rajgurr@users.noreply.github.com>
2022-11-07 12:03:17 +0530

    ----------

---
+++ Qustn-6.js
@@ -0,0 +1,14 @@
+let elemnt1=['hydrogen','H',1.008];
+let elemnt2=['helium','He',4.003];
+let elemnt3=['iron','Fe',55.85];
+let tabl=[];
+tabl.push(elemnt1);
+tabl.push(elemnt2);
+tabl.push(elemnt3);
+console.log(tabl);
+console.log(tabl[1]);
+console.log(tabl[1][1]);
+console.log(tabl[0][2]);
+console.log(tabl[1][0]);
+console.log(tabl[2][1]);
+      
