b5e188411fa612b1c1be3f3ba5553c75186946ec
Rajaguru_241 <86597128+rajgurr@users.noreply.github.com>
2022-11-07 11:54:59 +0530

    --------

---
+++ Qustn-5.js
@@ -0,0 +1,22 @@
+let str ='In space, no one can hear you code.';
+console.log(str.split());
+console.log(str.split('e'));
+console.log(str.split(' '));
+consol.log(str.split(''));
+
+let ary=['B','n','n','5'];
+console.log(ary.join());
+
+console.log(ary.join('a'));
+
+console.log(ary.join(' '));
+
+console.log(ary.join(''));
+
+let str2='water,space suits,food,plasma sword,batteries';
+
+let nstr = str2.split('');
+console.log(nstr);
+
+let nwstr= nstr.join('');
+console.log(nwstr);
